05445cc58f8af50e30fcf9baad363d2948d08d08
Rijal_Erkin <ricalibrahimli@gmail.com>
2020-04-16 15:08:32 +0400

    just for test

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SelfTest/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import  SelfTest from './components/SelfTest'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SelfTest
   }
 }
 </script>
---
+++ src/components/SelfTest.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="first-title">
+         <h1 class="title">{{ title }}</h1>
+         <p class="text1">{{ text1 }}</p>
+         <p class="">just </p>
+    </div>
+   
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+           title: 'i am component',
+           text1: ' lorem ipsum'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.first-title{
+    display: flex;
+    justify-content: center;
+}
+.title{
+    display: flex;
+    justify-content: center;
+}
+.text1{
+    display: flex;
+    justify-content: center;
+}
+</style>
